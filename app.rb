384cca59ed3876f6cd323d3e141894345472ca7b
pengyuchen <pengyu@nlplab.cc>
2016-04-07 22:17:44 +0800

    add messages route

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
-require "sinatra"
-require "json"
+require 'sinatra'
+require 'yaml'
+require 'json'
+
+require_relative 'models/messages'
 
 class MessengerAPI < Sinatra::Base
 
-get '/' do
-  'Messenger is up and running'
+  get '/' do
+    'Messenger service is up and running at /api/v1'
+  end
+
+  get '/api/v1/messages/?' do
+  	Message.new.to_json
+  end
+
+  get '/api/v1/messages/:id.json' do
+    Message.new(params[:id]).to_json
+  end
 end
-
-
-
-end
-
-
