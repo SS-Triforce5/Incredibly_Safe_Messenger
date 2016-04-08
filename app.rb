12c5361f7e10498fd1d135f19bd34191c7f0bcd8
pengyuchen <pengyu@nlplab.cc>
2016-04-08 18:42:45 +0800

    refactor and add post route to message table

--- app.rb
+++ app.rb
@@ -7,15 +7,28 @@ require_relative 'models/init'
 
 class MessengerAPI < Sinatra::Base
 
-  get '/' do
+  app_get_root = lambda do
     'Messenger service is up and running at /api/v1'
   end
 
-  get '/api/v1/messages/?' do
+  app_get_all_messages = lambda do
     Message.all.to_json
   end
 
-  get '/api/v1/messages/:id.json' do
+  app_get_someone_messages = lambda do
     Message.where(sender: params[:id]).or(receiver: params[:id]).all.to_json
   end
+
+  app_post_messages = lambda do  
+    Message.create(JSON.parse(request.body.read))
+  end
+
+
+
+
+  # Web App Views Routes
+  get '/', &app_get_root
+  get '/api/v1/messages/?', &app_get_all_messages
+  post '/api/v1/messages/?', &app_post_messages
+  get '/api/v1/messages/:id.json', &app_get_someone_messages
 end
