97045f98d974dec486262e09e6b90293f82b36ba
pengyuchen <pengyu@nlplab.cc>
2016-04-08 18:54:31 +0800

    add channel schema

---
+++ models/channel.rb
@@ -0,0 +1,17 @@
+class Channel < Sequel::Model
+  plugin :timestamps, :update_on_create => true
+  # def to_json(options = {})
+  #   JSON.pretty_generate(
+  #     { 
+  #       type: 'message',
+  #       id: id,
+  #       data: {
+  #         sender: sender,
+  #         receiver: receiver,
+  #         time: created_at
+  #       }
+  #     },
+  #     options
+  #   )
+  # end
+end
