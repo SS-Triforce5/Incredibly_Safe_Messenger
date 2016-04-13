60d96283406ad50ccb22ca3d49f3dd50d4ac042f
Kuan Yu Chen <chenky0916@gmail.com>
2016-04-13 17:04:06 +0800

    add testing(spec)

---
+++ specs/message_spec.rb
@@ -0,0 +1,60 @@
+require_relative './spec_helper'
+
+describe 'Testing Message resource routes' do
+  before do
+    User.dataset.delete
+    Channel.dataset.delete
+    Message.dataset.delete
+  end
+
+  describe 'Creating new Messages' do
+    it 'HAPPY: should create a new Message' do
+      req_header = { 'CONTENT_TYPE' => 'application/json' }
+      req_body = { sender: 'Demo Sender' , receiver: 'Demo Receiver' }.to_json
+      post '/api/v1/message/', req_body, req_header
+      _(last_response.status).must_equal 201
+      _(last_response.location).must_match(%r{http://})
+    end
+
+    it 'SAD: should not create Messages with null sender and receiver' do
+      req_header = { 'CONTENT_TYPE' => 'application/json' }
+      req_body = { sender:null, receiver:null}.to_json
+      post '/api/v1/message/', req_body, req_header
+      post '/api/v1/message/', req_body, req_header
+      _(last_response.status).must_equal 400
+      _(last_response.location).must_be_nil
+    end
+  end
+
+  describe 'Finding existing messages' do
+    it 'HAPPY: should find an existing message' do
+      new_message = Message.create(name: 'demo Message')
+      new_configs = (1..3).map do |i|
+        new_project.add_configuration(filename: "Message_file#{i}.rb")
+      end
+
+      get "/api/v1/message/#{new_message.id}"
+      _(last_response.status).must_equal 200
+
+      results = JSON.parse(last_response.body)
+      _(results['data']['id']).must_equal new_message.id
+      3.times do |i|
+        _(results['relationships'][i]['id']).must_equal new_configs[i].id
+      end
+    end
+
+    it 'SAD: should not find non-existent projects' do
+      get "/api/v1/message/#{invalid_id(Message)}"
+      _(last_response.status).must_equal 404
+    end
+  end
+
+  describe 'Getting an index of existing Messages' do
+    it 'HAPPY: should find list of existing Messages' do
+      (1..5).each { |i| Message.create(name: "Message #{i}") }
+      result = get '/api/v1/message'
+      mes = JSON.parse(result.body)
+      _(mes['data'].count).must_equal 5
+    end
+  end
+end
