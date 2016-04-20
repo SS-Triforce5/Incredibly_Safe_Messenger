7c81a9d5dcc99d50453b030cf3a35e9d26f02408
System Administrator <root@soutekiMacBook-Air.local>
2016-04-21 01:33:38 +0800

    Message encryption done

--- models/message.rb
+++ models/message.rb
@@ -1,9 +1,23 @@
 require 'json'
 require 'sequel'
+require 'base64'
+require_relative'lib/encryptable_model'
 class Message < Sequel::Model
+  include EncryptableModel
   plugin :timestamps, :update_on_create => true
-  set_allowed_columns :sender, :receiver, :message
+  set_allowed_columns :sender, :receiver
+
+def message=(message_plaintext)
+  @message = message_plaintext
+  self.message_encrypted = encrypt(@message)
+end
+
+def message
+  @message ||= decrypt(message_encrypted)
+end
+
   def to_json(options = {})
+    msg = message ? Base64.strict_encode64(message) : nil
     JSON.pretty_generate(
       {
         type: 'message',
@@ -11,6 +25,7 @@ class Message < Sequel::Model
         data: {
           sender: sender,
           receiver: receiver,
+          message_base64: msg,
           time: created_at
         }
       },
