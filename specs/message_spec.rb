576cbc802de7b37e81b2d826b7c7b02395c909d8
pengyuchen <pengyu@nlplab.cc>
2016-04-25 09:13:29 +0800

    all spec except for account done

--- specs/message_spec.rb
+++ specs/message_spec.rb
@@ -31,7 +31,7 @@ describe 'Testing Message resource routes' do
       new_message = Message.create(sender: 'Kuan' ,receiver: 'pengyuchen')
       new_message.message = 'hello~~~~~'
       new_message.save
-      get "/api/v1/message/#{new_message.id}"
+      get "/api/v1/message/#{new_message.sender}"
       _(last_response.status).must_equal 200
       results = JSON.parse(last_response.body)
       _(results[0]['id']).must_equal new_message.id
