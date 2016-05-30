4d6eddef8739de6333b5962cd0396e681c2c58ff
pengyuchen <pengyu@nlplab.cc>
2016-05-30 10:53:55 +0800

    reorder all message output

--- services/find_all_account_message.rb
+++
@@ -1,8 +0,0 @@
-# Find all projects (owned and contributed to) by an account
-class FindAllAccountMessages
-  def self.call(account)
-    my_messages = Message.where(sender: account.id).all
-    received_message = Message.where(receiver: account.id).all
-    my_messages + received_message
-  end
-end
