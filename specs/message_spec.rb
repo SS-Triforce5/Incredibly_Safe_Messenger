92cee7e09c1be88f8138eea6ba787ba6410b92a3
conkytw <chenky0916@gmail.com>
2016-06-14 14:06:45 +0800

    modified spec (has bug)

--- specs/message_spec.rb
+++ specs/message_spec.rb
@@ -27,12 +27,15 @@ describe 'Testing Message resource routes' do
   end
 
   describe 'Finding all messages of one user' do
+    @my_account = create_client_account(username: 'Kuan', email:'abc@gmail.com',password:'123')
+    @other_account = create_client_account(username: 'Edward', email:'def@gmail.com',password:'777')
+    @other_account2 = create_client_account(username: 'Tim', email:'ya@gmail.com',password:'888')
+    msg = CreateNewMessageFromSender.call(sender: 'Kuan' , receiver: 'Edward',message: 'Hi')
+    msg2 = CreateNewMessageFromSender.call(sender: 'Kuan' , receiver: 'Tim',message: 'Hello!')
     it 'HAPPY: should find all existing message of one user' do
-      act1 = CreateNewAccount.call(username: 'Kuan', email:'abc@gmail.com',password:'123')
-      act2 = CreateNewAccount.call(username: 'Edward', email:'def@gmail.com',password:'123')
-      msg = CreateNewMessageFromSender.call(sender: 'Kuan' , receiver: 'Edward',message: 'Hi')
-      msg2 = CreateNewMessageFromSender.call(sender: 'Edward' , receiver: 'Kuan',message: 'Hello!')
-      get "/api/v1/message/Kuan"
+      auth_token = authorized_account_token(username: 'Kuan', password: '123')
+      get "/api/v1/message/#{@my_account.username}" ,nil,
+        'HTTP_AUTHORIZATION' => "Bearer #{auth_token}"
       _(last_response.status).must_equal 200
       results = JSON.parse(last_response.body)
       _(results['data'].count).must_equal 2
@@ -44,6 +47,22 @@ describe 'Testing Message resource routes' do
     end
   end
 
+  describe 'Finding the message between two users' do
+    @my_account = create_client_account(username: 'Kuan', email:'abc@gmail.com',password:'123')
+    @other_account = create_client_account(username: 'Edward', email:'def@gmail.com',password:'777')
+    msg = CreateNewMessageFromSender.call(sender: 'Kuan' , receiver: 'Edward',message: 'Hi')
+    msg2 = CreateNewMessageFromSender.call(sender: 'Edward' , receiver: 'Kuan',message: 'Good morning!')
+    msg3 = CreateNewMessageFromSender.call(sender: 'Kuan' , receiver: 'Edward',message: 'Its raining')
+
+    it 'HAPPY: should find all messages between two users' do
+      auth_token = authorized_account_token(username: 'Kuan', password: '123')
+      get "/api/v1/message/#{@my_account.username}/#{@other_account.username}" ,nil,
+        'HTTP_AUTHORIZATION' => "Bearer #{auth_token}"
+      _(last_response.status).must_equal 200
+      results = JSON.parse(last_response.body)
+      _(results['data'].count).must_equal 3
+    end
+  end
 
 
 end
